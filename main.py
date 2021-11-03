09506b9274f4af012de87569909d36e6e5ee7c90
Leo Lu <luyuxuanleo@gmail.com>
2021-11-03 17:31:49 +0800

    Support for videos, image captions and scope filters.

--- main.py
+++ main.py
@@ -14,11 +14,14 @@ tg_bot_token = config['tg_bot_token']
 channel_chat_id = config['channel_chat_id']
 pm_chat_id = config['pm_chat_id']
 mastodon_host = config['mastodon_host']
-mastodon_api_secret = config['mastodon_api_secret']
+mastodon_api_access_token = config['mastodon_api_access_token']
 mastodon_username = config['mastodon_username']
 
 bot = telebot.TeleBot(tg_bot_token, parse_mode=None)
 
+@bot.message_handler()
+def log(message):
+    print(message.chat.id, message)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
@@ -29,18 +32,25 @@ def send_welcome(message):
 def send_message(content):
     try:
         toot = json.loads(content['payload'])
-        if toot['account']['username'] == mastodon_username and toot['in_reply_to_id'] is None:
+        print(json.dumps(toot, ensure_ascii=False, indent=2))
+        if toot['account']['username'] == mastodon_username and toot['in_reply_to_id'] is None and toot['visibility'] in config['scope']:
             txt = markdownify(toot['content'])
+            txt += 'from: ' + toot['url']
+            print(txt)
             if len(toot['media_attachments']) != 0:
                 medias = []
                 for i in toot['media_attachments']:
-                    medias.append(types.InputMediaPhoto(i['url']))
+                    if i['type'] == 'image':
+                        medias.append(types.InputMediaPhoto(i['url']))
+                    elif i['type'] == 'video':
+                        medias.append(types.InputMediaVideo(i['url']))
+                medias[0].caption = txt
                 bot.send_media_group(channel_chat_id, medias)
-            txt += 'from: ' + toot['url']
-            print(txt)
-            bot.send_message(channel_chat_id, txt, disable_web_page_preview=True)
+            else:
+                bot.send_message(channel_chat_id, txt, disable_web_page_preview=True)
     except Exception as e:
         print(e)
+        bot.send_message(pm_chat_id, f"Exception: {e}")
 
 
 def on_message(ws, message):
@@ -57,8 +67,7 @@ def on_error(ws, error):
 def websocketTest():
     websocket.enableTrace(True)
     ws = websocket.WebSocketApp(
-        "wss://{host}/api/v1/streaming?access_token={token}&stream=user".format(host=mastodon_host,
-                                                                                token=mastodon_api_secret),
+        f"wss://{mastodon_host}/api/v1/streaming?access_token={mastodon_api_access_token}&stream=user",
         on_message=on_message,
         on_error=on_error)
     ws.run_forever()
